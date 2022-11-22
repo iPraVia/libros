69d8eaf634de24e5218459e873f8f8189adcf454
iPraVia <igpradin@gmail.com>
2022-11-22 14:19:31 -0400

    models

--- libros/app/models.py
+++ libros/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoUsuario(models.Model):
+    nombre = models.CharField(max_length=10,null=False,default='')
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=10);
+    email = models.EmailField();
+    password = models.CharField(max_length=10);
+    tipoUsuario = models.ForeignKey(TipoUsuario,on_delete=models.CASCADE,null=True)
+    estado = models.BooleanField()
+
+class Libro(models.Model):
+    pass
